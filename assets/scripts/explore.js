0eb1cd0c7dd5f03cd3bce8641735f4735537e158
hanghanghappy <chc063@ucsd.edu>
2022-10-30 23:48:56 -0700

    Updated explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,42 +8,32 @@ function init() {
   let voiceSelect = document.getElementById("voice-select");
   let voices = [];
 
-  function populateVoiceList() {
+  synth.addEventListener("voiceschanged", function(e){
     voices = synth.getVoices();
     for (let i = 0; i < voices.length ; i++) {
-      let option = document.createElement('option');
+      let option = document.createElement("option");
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
-  
-      if (voices[i].default) {
-        option.textContent += ' — DEFAULT';
-      }
-  
-      option.setAttribute('data-lang', voices[i].lang);
-      option.setAttribute('data-name', voices[i].name);
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
       voiceSelect.appendChild(option);
     }
-  }
-  populateVoiceList();
-  if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
-  }
+  })
 
   let speechButton = document.querySelector("button");
   let speechText = document.getElementById("text-to-speak");
   let speechImage = document.querySelector("img");
   speechButton.addEventListener("click", function(e){
-    let utterThis = new SpeechSynthesisUtterance(speechText.value);
-    let selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    let utter = new SpeechSynthesisUtterance(speechText.value);
     for (let i = 0; i < voices.length ; i++) {
-      if (voices[i].name === selectedOption) {
-        utterThis.voice = voices[i];
+      if (voices[i].name === voiceSelect.selectedOptions[0].getAttribute("data-name")) {
+        utter.voice = voices[i];
       }
     }
-    synth.speak(utterThis);
+    synth.speak(utter);
     if (synth.speaking == true){
       speechImage.src = "assets/images/smiling-open.png";
     }
-    utterThis.addEventListener("end", function(e){
+    utter.addEventListener("end", function(e){
       speechImage.src = "assets/images/smiling.png";
     })
   })
